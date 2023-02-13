81a21eed59161c4f577a375e882995f25811c700
Alan Ruiz <ruizflavio@gmail.com>
2023-02-13 07:32:28 -0300

    v2

--- regedit.py
+++ regedit.py
@@ -1,14 +1,18 @@
 import winreg
-
        
 path = winreg.HKEY_CURRENT_USER
 
-software = winreg.OpenKeyEx(path, r"SOFTWARE\\")
+software = winreg.OpenKeyEx(path, r"SOFTWARE\Elea\Elea2\\")
+open_key = winreg.CreateKey(software, "ELea3")
+winreg.SetValueEx(open_key, "VALOR ", 0, winreg.REG_SZ, "122333")
+
+
 
 old_Key = winreg.DeleteKey(software, "Elea")
 new_key = winreg.CreateKey(software, "ELea")
 
 winreg.SetValueEx(new_key, "VALOR ACTUAL !!!!!!!!!!!!!!!!", 0, winreg.REG_SZ, "0")
 
-if new_key:
-    winreg.CloseKey(new_key)
+# if new_key:
+#     winreg.CloseKey(new_key)
+#Commit
